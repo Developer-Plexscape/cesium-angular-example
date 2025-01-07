5e9ae1980dd863628c5336637b310d55705eff58
Aristeidis Bampakos <abampakos@plexscape.com>
2025-01-07 10:45:31 +0200

    Upgrade to Angular 19 and Cesium 1.125

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 import { CesiumDirective } from './cesium.directive';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, CesiumDirective],
+  imports: [CesiumDirective],
   template: '<div appCesium></div>'
 })
 export class AppComponent {}
